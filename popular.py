ce514960b88f8a0c16b8e605916006e97f5c16a5
sparsazad <sparsazad@pbsc.com>
2019-04-10 10:08:30 -0400

    corrected popular.py

--- popular.py
+++ popular.py
@@ -9,10 +9,10 @@ with open('package_info.json', 'r') as f:
     data = json.load(f)
 
 # can filter by keyword in the description
-data = [item for item in data if 'video' in item['desc']]
+# data = [item for item in data if 'video' in item['desc']]
 data.sort(key=install_sort, reverse=True)
 
 # can take only top 5
 # data_str = json.dumps(data[:5], indent=2)
 data_str = json.dumps(data, indent=2)
-print(data_Str)
+print(data_str)
